25474372aa877d85d0e99616ae81f0a68c1fb88d
carlosmartins77 <carlosmatrins@gmail.com>
2022-11-14 20:03:07 +0000

    ADD localstorage login

--- Code/Register/script.js
+++ Code/Register/script.js
@@ -1,43 +1,46 @@
 const { Console } = require("console");
 
-async function login(){
+async function login() {
 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
     console.log(username, password)
     const url = 'http://localhost:7070/auth/login'
-    const data = { 
+    const data = {
         username,
         password
     }
 
     const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
-    }).then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((err) => {
-      console.log(err);
-    });
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+        }).then((response) => response.json())
+        .then((data) => {
+            console.log("Token: ", data.token);
+            localStorage.setItem("token", data.token)
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 
     // TODO COLOCAR DEPOIS NOS COOKIES O TOKEN
 
     return true;
 }
 
-async function register(){
+async function register() {
 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     const email = document.getElementById("email").value;
     const name = document.getElementById("nome").value;
-    
+
     const url = 'http://localhost:7070/auth/register'
-    const data = { 
+    const data = {
         username,
         password,
         email,
@@ -45,16 +48,16 @@ async function register(){
     }
     console.log(username, password, email, name)
     const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
-    }).then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((err) => {
-      console.log(err);
-    });
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+        }).then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((err) => {
+            console.log(err);
+        });
 
     return true;
 }
